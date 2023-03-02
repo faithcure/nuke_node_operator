f551979a0ba9e27002483ea5ed8c1d3ea980895e
dkeraudren <damien.keraudren@gmail.com>
2023-03-02 15:12:06 +0100

    Fixed menu path for tool consitency

--- menu.py
+++ menu.py
@@ -3,13 +3,14 @@ import node_operator_main_v01
 import importlib
 
 
-toolbar = nuke.menu("Nodes")
-i = toolbar.addMenu("Studio")
+# toolbar = nuke.menu("Nodes")
+# i = toolbar.addMenu("Studio")
+i = nuke.menu("Nuke")
 
 # development node operator
 def start_NOP():
     # node operator
     importlib.reload(node_operator_main_v01)
     node_operator_main_v01.start()
-i.addCommand("Node Operator v0.1", "start_NOP()")
+i.addCommand("CGEV Tools/Node Operator v0.1", "start_NOP()", "alt+o")
 
